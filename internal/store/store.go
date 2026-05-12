d76a2373b723012b39ba23f8f4d4ab78dd2d8daa
Sam Potter <spotter1642@gmail.com>
2026-05-12 13:06:40 -0400

    Remove user related db methods and tables

--- internal/store/store.go
+++ internal/store/store.go
@@ -2,18 +2,11 @@ package store
 
 import (
 	"context"
-	"errors"
 
 	"github.com/stpotter16/hours/internal/types"
 )
 
-var ErrUserNotFound = errors.New("user not found")
-
 type Store interface {
-	// Users
-	GetUserByUsername(ctx context.Context, username string) (types.User, error)
-	CreateUser(ctx context.Context, username, passwordHash string, isAdmin bool) error
-
 	// Clicks
 	GetClicks(ctx context.Context) ([]types.Click, error)
 	CreateClick(ctx context.Context) (int, error)
